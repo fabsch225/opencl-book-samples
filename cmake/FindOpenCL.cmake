0c7b4cf163e270ca1dd05e31e005054712d4460e
Fabian Schuller <fabianschuller2005@gmail.com>
2025-08-09 14:28:04 +0200

    ADDED intel gpu support for linux

--- cmake/FindOpenCL.cmake
+++ cmake/FindOpenCL.cmake
@@ -58,20 +58,36 @@ ELSE (APPLE)
 	    FIND_PATH(_OPENCL_CPP_INCLUDE_DIRS CL/opencl.hpp PATHS "${_OPENCL_INC_CAND}" $ENV{CUDA_INC_PATH} $ENV{CUDA_PATH}/include)
 	
 	ELSE (WIN32)
+		# Unix style platforms
+		FIND_LIBRARY(OPENCL_LIBRARIES OpenCL
+				PATHS /usr/lib/x86_64-linux-gnu
+				ENV LD_LIBRARY_PATH
+		)
 
-            # Unix style platforms
-            FIND_LIBRARY(OPENCL_LIBRARIES OpenCL
-              ENV LD_LIBRARY_PATH
-            )
+		GET_FILENAME_COMPONENT(OPENCL_LIB_DIR ${OPENCL_LIBRARIES} PATH)
+		GET_FILENAME_COMPONENT(_OPENCL_INC_CAND ${OPENCL_LIB_DIR}/../../include ABSOLUTE)
 
-            GET_FILENAME_COMPONENT(OPENCL_LIB_DIR ${OPENCL_LIBRARIES} PATH)
-            GET_FILENAME_COMPONENT(_OPENCL_INC_CAND ${OPENCL_LIB_DIR}/../../include ABSOLUTE)
+		FIND_PATH(OPENCL_INCLUDE_DIRS CL/cl.h
+				PATHS
+				${_OPENCL_INC_CAND}
+				/usr/include
+				/usr/local/include
+		)
 
-            # The AMD SDK currently does not place its headers
-            # in /usr/include, therefore also search relative
-            # to the library
-            FIND_PATH(OPENCL_INCLUDE_DIRS CL/cl.h PATHS ${_OPENCL_INC_CAND} "/usr/local/cuda/include")
-            FIND_PATH(_OPENCL_CPP_INCLUDE_DIRS CL/opencl.hpp PATHS ${_OPENCL_INC_CAND} "/usr/local/cuda/include")
+		FIND_PATH(_OPENCL_CPP_INCLUDE_DIRS CL/opencl.hpp
+				PATHS
+				${_OPENCL_INC_CAND}
+				/usr/include
+				/usr/local/include
+		)
+
+		# The AMD SDK currently does not place its headers
+		# in /usr/include, therefore also search relative
+		# to the library
+		FIND_PATH(OPENCL_INCLUDE_DIRS CL/cl.h PATHS ${_OPENCL_INC_CAND} "/usr/local/cuda/include")
+		FIND_PATH(_OPENCL_CPP_INCLUDE_DIRS CL/opencl.hpp PATHS ${_OPENCL_INC_CAND} "/usr/local/cuda/include")
+
+
 
 	ENDIF (WIN32)
 
